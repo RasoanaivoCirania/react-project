b3cfd3849e82f4870f3a634f1392e806e6aec582
AinaR <ainarrasoanaivo@gmail.com>
2025-10-23 21:26:16 +0300

    Adding component and routes

---
+++ src/pages/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga minima
+      ullam harum laboriosam aliquam quasi eos libero cumque, nesciunt
+      molestiae.
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime,
+        veniam? Accusantium, excepturi! Iste eaque iusto cum expedita provident.
+        Doloremque dolore voluptates sit optio, earum obcaecati suscipit vero
+        quam ullam officia quia, dicta eius recusandae animi voluptas quo!
+        Repellendus, facilis reprehenderit?
+      </p>
+    </div>
+  );
+};
+
+export default About;
